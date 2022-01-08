8e488a0ae02d3054fd1866de2f80b21eb7427d86
Anders <1086088@ucn.dk>
2022-01-08 01:23:32 +0100

    full v2

---
+++ src/api/dagens-traening/controllers/dagens-traening.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  dagens-traening controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::dagens-traening.dagens-traening');
---
+++ src/api/dagens-traening/routes/dagens-traening.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * dagens-traening router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::dagens-traening.dagens-traening');
